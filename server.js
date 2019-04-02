3dc0f6d83a1842a660d30756e41287408377e617
Shubham Rastogi <shubham.rastogi@attivio.com>
2019-04-01 23:49:26 -0400

    rectified build failure

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, "/dist/phonebook-angular")));
+app.use(express.static(__dirname + "/dist/phonebook-angular"));
 
-app.get("/*", function(req, res) {
+app.get("/*", (req, res) => {
 
-  res.sendFile(express.static(path.join(__dirname, "/dist/phonebook-angular/index.html")));
+  res.sendFile(path.join(__dirname + "/dist/phonebook-angular/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
